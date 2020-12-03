a9102eab340515921bed497a3909c51a98b43770
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2020-12-03 12:19:23 +0100

    add actual days of the week, add error message when city is not found

--- script.js
+++ script.js
@@ -6,15 +6,22 @@
     let temp = document.getElementById("temp");
     let weatherPage = document.getElementById("weather");
     let windPage = document.getElementById("wind");
-    let fiveDayForecast = document.getElementById("5days");
-    //let key = 6d62f680b47ba0a60f39206b9e1a714a;
+    let fiveDaysTemp = document.getElementById("5daysTemp");
+    let fiveDaysWeather = document.getElementById("5daysWeather");
+    let weekdayHTML = document.getElementById("weekday");
     let longitude;
     let latitude;
     let currentTemp;
     let weather;
     let wind;
     let icon;
-    let forecast = [];
+    let forecastTemp = [];
+    let forecastWeather = [];
+    let d = new Date();
+    let day = d.getDay();
+    let weekday = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+    let weekdayTable = [];
+
 
 
     //hide image until city and forecast is loaded
@@ -26,28 +33,39 @@
         fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&appid=6d62f680b47ba0a60f39206b9e1a714a")
             .then(
                 function(response1) {
-
+                    if(response1.status !== 200){
+                        document.getElementById("error").innerHTML = "I don't know this city :( <br> Try again please"
+                    }
                     response1.json().then(function(data1) {
                         latitude = data1.city.coord.lat;
                         longitude = data1.city.coord.lon;
-                        forecast.length = 0;
+                        //clear the arrays with 5 day forecast information so it's reset when a new city is entered
+                        forecastTemp.length = 0;
+                        forecastWeather.lenght = 0;
+                        weekdayTable.length = 0;
                         fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&exclude=hourly,minutely&appid=6d62f680b47ba0a60f39206b9e1a714a")
                             .then(
                                 function(response2){
                                     response2.json().then(function(data2){
+                                        //get data for current moment for the city that's entered
                                         console.log(data2);
-                                        currentTemp = data2.daily[0].temp.day;
-                                        weather = data2.daily[0].weather[0].main;
+                                        currentTemp = data2.current.temp;
+                                        weather = data2.daily[0].weather[0].description;
                                         wind = data2.daily[0].wind_speed;
                                         icon = data2.daily[0].weather[0].icon;
 
+                                        //get info for the next 5 days and push them to an array
                                         for(i = 1; i < 6; i ++){
                                             let temperature = Math.round(data2.daily[i].temp.day);
-                                            let span = "<li>" + temperature + " C" + "</li>"
-                                            forecast.push(span);
+                                            let weather5days = data2.daily[i].weather[0].main;
+                                            let spanTemp = "<td>" + temperature + " C" + "</td>"
+                                            let spanWeather = "<td>" + weather5days + "</td>"
+                                            forecastTemp.push(spanTemp);
+                                            forecastWeather.push(spanWeather);
                                         }
 
                                         currentWeather();
+                                        forecastFivedays();
 
                                     });
                                 }
@@ -65,8 +83,31 @@
         weatherPage.innerText = weather;
         windPage.innerText = Math.round((wind / 1000) * 3600) + "k/ph";
         img.src = "img/" + icon + ".png";
-        fiveDayForecast.innerHTML = forecast.join("");
 
     };
+
+    forecastFivedays = () => {
+        //push days of the week to array
+        let spanWeekday;
+        for(i = 0; i < 5; i++){
+            if(day === 6){
+                day = 0;
+                spanWeekday = "<td>" + weekday[0] + "</td>"
+                weekdayTable.push(spanWeekday);
+            }
+            else{
+                day = day +1;
+                spanWeekday = "<td>" + weekday[day] + "</td>"
+                weekdayTable.push(spanWeekday);
+            }
+        }
+
+        //add the info to the html
+        weekdayHTML.innerHTML = weekdayTable.join("");
+        fiveDaysTemp.innerHTML = forecastTemp.join("");
+        fiveDaysWeather.innerHTML = forecastWeather.join("");
+
+
+    }
 
 })();
